b37462269d4897f832fa4f706b01e23afd0a3b22
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-21 08:49:25 +0900

    lesson09

--- exam/exam.js
+++ exam/exam.js
@@ -7,6 +7,7 @@
     if (countdown < 0){
       clearInterval(intervalId);
       countdown = 3 * 1000;
+      btn.disabled = false;
     }
 
     const totalSeconds  = Math.floor(countdown / 1000);
@@ -14,7 +15,11 @@
     const minutes = Math.floor(totalSeconds/60);
     const seconds = totalSeconds % 60;
 
-    timer.textContent = `${minutes}:${seconds}`;
+    const minutesFormatted = String(minutes).padStart(2, '0');
+    const secondsFormatted = String(seconds).padStart(2, '0');
+
+
+    timer.textContent = `${minutesFormatted}:${secondsFormatted}`;
   }
 
   const timer = document.getElementById('timer');
@@ -24,6 +29,7 @@
 
   btn.addEventListener('click', ()=>{
     endTime = new Date().getTime() + 3 * 1000;
+    btn.disabled = true;
     intervalId = setInterval(check, 100);
   });
 }
